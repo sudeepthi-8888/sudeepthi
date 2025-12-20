3f6b3d225a50914236e4c2ea9db0d29850a04ffb
sudeepthi-8888 <sriharshithalakshmisudeepthi@gmail.com>
2025-12-20 06:46:19 +0530

    Add files via upload

---
+++ distance travelled by object.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+int main() 
+ { 
+    double a, b, c, d; 
+    double result; 
+    // Input values for a, b, c, and d 
+    printf("Enter the value of a: "); 
+    scanf("%lf", &a); 
+    printf("Enter the value of b: "); 
+    scanf("%lf", &b); 
+    printf("Enter the value of c: "); 
+    scanf("%lf", &c); 
+    printf("Enter the value of d: "); 
+    scanf("%lf", &d); 
+ 
+    // Evaluate the expression 
+    result = a / b * c - b + a * d / 3; 
+    // Output the result 
+    printf("Result: %.2lf\n", result); 
+    return 0; 
+} 
